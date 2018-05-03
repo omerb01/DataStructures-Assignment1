1ec38626cecd7df156f56d19589bba682ffc8352
Ilya <int3rsys@live.com>
2018-05-03 19:33:15 +0300

    UPDATE__

--- AVLTree.h
+++ AVLTree.h
@@ -3,9 +3,14 @@
 //
 
 #include <exception>
+#include <cmath>
+#include <rpcdce.h>
+#include <string>
 
 #ifndef DATASTRUCTURES_HW1_AVLTREE_H
 #define DATASTRUCTURES_HW1_AVLTREE_H
+
+using std::string;
 
 template<typename T>
 class AVLTree {
@@ -13,10 +18,15 @@ class AVLTree {
         T data;
         int key1;
         int key2;
+        Node *parent;
         Node *left;
         int h_left;
         Node *right;
         int h_right;
+
+        Node(T d, int k1, int k2) : data(d), key1(k1), key2(k2), left(nullptr),
+                                    right(nullptr), parent(nullptr), h_left(-1),
+                                    h_right(-1) {}
     };
 
     Node *root;
@@ -37,13 +47,57 @@ class AVLTree {
 public:
     AVLTree() : root(nullptr) {}
 
-    void insert(T data, int key1, int key2) {
-        Node* iterator=root;
-        if(root->key1 == key1 && root->key2 == key2) return;
-        else if (key1 < root->key1 ||
-                 (key1 == root->key1 && key2 > root->key2)){
+    bool insert(T data, int key1, int key2) {
+        Node *iterator = root;
+        string path="";
+        while (iterator->right != nullptr && iterator->left != nullptr) {
+            if (iterator->key1 == key1 && iterator->key2 == key2) break;
+            else if (key1 < iterator->key1 ||
+                     (key1 == iterator->key1 && key2 > iterator->key2)) {
+                iterator = iterator->left;
+            } else {
+                iterator = iterator->right;
+            }
+        }
+        if (iterator->key1 == key1 && iterator->key2 == key2) {
+            return false;
+        }
+        Node *new_node = new Node(data, key1, key2);
+        if (key1 < iterator->key1 ||
+            (key1 == iterator->key1 && key2 > iterator->key2)) {
+            iterator->left = new_node;
+        } else {
+            iterator->right = new_node;
+        }
+        new_node->parent = iterator;
+        Node *p, *v=new_node;
+        while (v != root) {
+            int height_p, height_v;
+            p = v->parent;
+            p->h_left > p->h_right ? height_p = p->h_left + 1 : height_p =
+                                                                        p->h_right +
+                                                                        1;
+            v->h_left > v->h_right ? height_v = v->h_left + 1 : height_v =
+                                                                        v->h_right +
+                                                                        1;
+            if(height_p >= height_v+1){
+                return true;
+            }
+            if(p->right==v){
+                path = "R" + path;
+                p->h_right++;
+            }else{
+                path = "L" + path;
+                p->h_left++;
+            }
+            if(abs(p->h_left - p->h_right)>1){
+                // switch case n' roll
+            }else{
+                v=p;
+            }
 
         }
+        return true;
     }
 };
 
