3d0f0564e4b870644048665d51a8c8e4407ae98a
Omer <omer.be@campus.technion.ac.il>
2018-05-03 16:55:36 +0300

    added exceptions file

---
+++ exceptions.h
@@ -0,0 +1,20 @@
+//
+// Created by Omer on 03/05/2018.
+//
+
+#include <exception>
+
+#ifndef DATASTRUCTURES_HW1_EXCEPTIONS_H
+#define DATASTRUCTURES_HW1_EXCEPTIONS_H
+
+#define NEW_EXCEPTION(exp, from) \
+    class exp : public from{\
+    public: \
+        virtual const char* what() const throw(){\
+            return #exp;\
+    }}
+
+NEW_EXCEPTION(AVLTreeException, std::exception);
+NEW_EXCEPTION(AVLMemoryError, AVLTreeException);
+
+#endif //DATASTRUCTURES_HW1_EXCEPTIONS_H
