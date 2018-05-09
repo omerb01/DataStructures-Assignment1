ff21a1dde4bc46ceb7e8f02af942bfdc80967cac
Ilya <int3rsys@live.com>
2018-05-09 19:03:37 +0300

    Ilya's part (UNFINISHED)

--- Oasis_test.cpp
+++ Oasis_test.cpp
@@ -6,6 +6,16 @@
 #include "testMacros.h"
 #include "exceptions.h"
 
+bool addPlayer(){
+    Oasis os();
+    ASSERT_EXCEPTION(Player p1(123,-1),OasisInvalidInput);
+    ASSERT_EXCEPTION(Player p1(-1,12),OasisInvalidInput);
+    ASSERT_NO_EXCEPTION(Player p1(123,12));
+    ASSERT_EXCEPTION(Player p1(123,12),OasisInvalidInput);
+    return true;
+}
+
 int main() {
+    RUN_TEST(addPlayer);
     return 0;
 }
